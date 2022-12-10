a795432c94f46d1455b6336e27cce54a0206a5d5
Georg <kubrakgeorg@gmail.com>
2022-12-10 14:38:35 +0500

    echo "# DZ.Java.2.2" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin git@github.com:GeorgKubrak/DZ.Java.2.2.git git push -u origin main

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int check = 1100;
+        int payment = 1001;
+
+        int bonus;
+        if (payment >= 1001) {
+            bonus = payment / 100;
+        } else {
+            bonus = 0;
+        }
+        int balance = check + payment + bonus;
+
+        System.out.println("Ваш счёт пополнен. Ваш баланс составляет " + balance + " рублей");
+        System.out.println("Вам начислено " + bonus + " рублей");
+    }
